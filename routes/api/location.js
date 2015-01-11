1f068ad76dc277fcb0feda4f147a90bed0d430e2
Andy Villiger <a.villiger@gmail.com>
2015-01-11 18:11:37 +0100

    proximety alarm via Google Cloud Messaging

--- routes/api/location.js
+++ routes/api/location.js
@@ -1,9 +1,38 @@
 "use strict";
 
+var _ = require('underscore');
 var express = require('express');
+var https = require('https');
 var models  = require('../../lib/models');
 var auth = require('../../lib/auth');
+var util = require('../../lib/util');
+var env = process.env.NODE_ENV || "development";
+var config = require(__dirname + '/../../config/config.json')[env];
 var router = express.Router();
+
+var sendGCM = function(data, callback) {
+    var options = {
+        host: 'android.googleapis.com',
+        port: 443,
+        path: '/gcm/send',
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": "key=" + config.google_apikey
+        }
+    };
+
+    var req = https.request(options, function(res) {
+        res.on('data', function(d) {
+            callback(null);
+        });
+    });
+    req.end(JSON.stringify(data));
+
+    req.on('error', function(e) {
+        callback(e);
+    });
+};
 
 // Push location
 router.post('/', auth.token, function(req, res) {
@@ -17,10 +46,54 @@ router.post('/', auth.token, function(req, res) {
         var latitude = req.body.latitude;
         var longitude = req.body.longitude;
 
+        // save old lat/lon in history
+        user.history.push({ latitude: user.latitude, longitude: user.longitude });
+
+        // set new lat/lon
         user.latitude = latitude;
         user.longitude = longitude;
-        user.history.push({ latitude: latitude, longitude: longitude });
         user.save();
+
+        for (var i = 0; i < user.friends.length; i++) {
+            var friend = user.friends[i];
+
+            var distance = util.distanceInKm(latitude, longitude, friend.latitude, friend.longitude);
+            if (distance <= 5) {
+                var lastPos = _.last(user.history.toJSON());
+                if (lastPos) {
+                    var lastDistance = util.distanceInKm(lastPos.latitude, lastPos.longitude, friend.latitude, friend.longitude);
+
+                    if (lastDistance > 5) {
+                        // send GCM to user
+                        sendGCM({
+                            registration_ids: user.clientIDs.toJSON(),
+                            data: {
+                                name: friend.name,
+                                email: friend.email,
+                                latitude: friend.latitude,
+                                longitude: friend.longitude
+                            }
+                        }, function(err) {
+                            console.log("GCM", err);
+                        });
+
+                        // send GCM to friend
+                        sendGCM({
+                            registration_ids: friend.clientIDs.toJSON(),
+                            data: {
+                                name: user.name,
+                                email: user.email,
+                                latitude: user.latitude,
+                                longitude: user.longitude
+                            }
+                        }, function(err) {
+                            console.log("GCM", err);
+                        });
+                    }
+                }
+
+            }
+        }
 
         res.send({ msg: "Position updated"});
     }
